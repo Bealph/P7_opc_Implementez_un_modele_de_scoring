446f76df13d128277353ddad8ddb9cbe5fa4ee37
Alpha <alfa.2.walidjan@gmail.com>
2023-12-21 14:25:13 +0100

    mis a jour fichier app.py, mon best model, top50, ensuite travaux sur test_app terminer

--- test_app.py
+++ test_app.py
@@ -1,19 +1,24 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import requests
-import json
 
 # URL de mon API Flask
-url = 'http://127.0.0.1:5000/prediction'  
+url = 'http://192.168.1.251:8501/prediction'  
 
 # Chargeons les données
-df = pd.read_csv(r"C:/Users/Microsoft/Desktop/Dossiers formation/Formation OC/Data science/P7/Donnees projet/P7_opc_Implementez_un_modele_de_scoring/top_50_train.csv", encoding='utf-8')
-df.drop(columns='TARGET', inplace=True)
-num_client = df.SK_ID_CURR.unique()
+df = pd.read_csv("top_50_train.csv", encoding='utf-8')
+df.set_index('SK_ID_CURR', inplace=True)
+print(df.head())
+
+num_client = df.index.unique()
 
 # Sélectionnez un client (par exemple, prenons le premier client dans cet exemple)
-client_data = df[df['SK_ID_CURR'] == num_client[0]].to_dict(orient='records')[0]
+client_data = df.loc[num_client[0]].to_dict()
 
-# Envoi de la requête HTTP au serveur Flask
+print(client_data)
+
+'''# Envoi de la requête HTTP au serveur Flask
 response = requests.post(url, json=client_data)
 
 # Vérification de la réponse
@@ -22,4 +27,4 @@ if response.status_code == 200:
     print("Prédictions de probabilité pour le client:", result['client_info'])
     print("Probabilités prédites:", result['prediction_proba'])
 else:
-    print("La requête a échoué avec le code:", response.status_code)
+    print("La requête a échoué avec le code:", response.status_code)'''
