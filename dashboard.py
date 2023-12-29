a25dcfd8272e753291e38095d088c99fdacdaba3
Alpha <alfa.2.walidjan@gmail.com>
2023-12-29 21:54:44 +0100

    (1ere tache effectuer dans le dashboard: header, image, texte expli, et footer)

--- dashboard.py
+++ dashboard.py
@@ -1,51 +1,77 @@
 import streamlit as st
 import pandas as pd
-from threading import Thread
 
-# Chargement des données (à remplacer par votre logique de chargement de données)
-data = pd.read_csv('votre_fichier.csv')
+menu_selector = st.sidebar.selectbox('Prediction probable sur le client',
+                                     ['proba'])
+col1 = st.columns
 
-# Fonction pour le dashboard Streamlit
-def streamlit_code():
-    st.title("Dashboard de Prédictions")
-    st.write("Ce dashboard permet d'accéder aux informations des clients et d'envoyer les prédictions à l'application.")
+st.markdown(
+    """
+<style>
+.centered {
+    display : flex;
+    justify-content : center;
+}
+</style>
 
-    # Dropdown pour sélectionner l'ID du client
-    selected_id = st.selectbox("Sélectionnez l'ID du client", data['id_client'].unique())
+<style>
+.header {
+    font-size : 24px;
+    text-align : center;
+    text-decoration : underline;
+}
+</style>
 
-    # Affichage des informations du client sélectionné sous forme de tableau (10 variables)
-    if selected_id is not None:
-        client_info = data[data['id_client'] == selected_id].head(1)  # Récupération des informations du client
-        st.write("Informations du client sélectionné :")
-        st.write(client_info)
+<style>
+.centered-text {
+    text-align : justify;
+    text-align-last : center;
+}
+</style>
 
-    # Bouton Submit pour envoyer les données et probabilités à l'application
-    if st.button('Submit'):
-        # Logique pour calculer les probabilités (remplacez par votre logique de prédiction)
-        proba_predictions = [0.75, 0.25, 0.8]  # Exemple de probabilités
-        # Envoi des probabilités à l'application (à remplacer par votre logique d'envoi)
-        # send_data_to_app(selected_id, proba_predictions)
-        
-        # Affichage des probabilités prédites
-        st.write("Probabilités prédites :")
-        st.write(proba_predictions)
+<style>
+.footer {
+    position : fixed;
+    bottom : 0;
+    left : 0;
+    width : 100%;
+    background-color : white;
+    text-align : justify;
+    text-align-last : center;
+    color : blue;
+    font-size : 8px;
+    
+    p {
+        margin-bottom: 0px;
+    }
+}
+</style>
 
-# Fonction pour exécuter le dashboard Streamlit dans un thread
-def streamlit_thread():
-    st.button("Lancer l'application")
-    thread = Thread(target=streamlit_code)
-    thread.start()
+""",
+unsafe_allow_html=True
+)
 
-if __name__ == '__main__':
-    streamlit_thread()
+st.markdown(
+    '<div class="centered"><img src="/img_test.PNG"></div>',
+    unsafe_allow_html=True
+)
 
+#st.header('Tableau de bord en temps réel')
+st.markdown(
+    '<h1 class="header">Tableau de bord en temps réel</h1>',
+    unsafe_allow_html=True
+)
 
+st.markdown(
+    '<p class="centered-text">Cette application vise à fournir un système de scoring avancé permettant\
+        l\'accès aux informations essentielles des clients. Grâce à des modèles prédictifs, elle offre la possibilité\
+            de générer des predictions pertinentes en temps réel.<br> <br> L\'objectif principale est de faciliter \
+                la prise de décision en fournissant des analysées détaillées basées sur les données des clients.</p>',
+    unsafe_allow_html=True
+)
 
-
-
-'''
-redesign du dash (image, texte d'expli, en quoi va concerner le dash, 5 a 6 ligne,  un dropdown(menu : id des clients, qui va permettre qd on clique sur l'id, ça affiche sous forme de tableau (max: 10 variables les plus important)
- les info du client), un bouton submit (qd on clique ça puisse envoyer les proba, les données à l'app, créer une fonction, en utilisant comme parametre les id client), et afficher dans le dashboard la liste des proba predict)
-
-
-'''
+st.markdown(
+    '<div class="footer"> <p>2023 DIALLO Alpha</p>\
+        <p>P7 OPC</p>\
+        </div>', unsafe_allow_html=True
+)
